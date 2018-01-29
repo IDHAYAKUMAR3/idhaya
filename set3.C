92a950c7ba0d249e6741bc5ce8e8fcb63232ed13
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-01-29 13:28:06 +0530

    Update set3.C

--- set3.C
+++ set3.C
@@ -2,11 +2,12 @@
 int main()
 {
 char ch;
+clrscr();
 printf("Enter the character to check Vowel or Consonant");
 scanf("%c",&ch);
 if(ch=='a',ch=='A',ch=='e',ch=='E',ch=='i',ch=='I',ch=='o',ch=='O',ch=='u',ch=='U')
 printf("the character is a Vowel");
 else 
 printf("the character is a Consonant");
-return 0;
+getch();
 }
