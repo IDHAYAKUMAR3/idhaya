f89601112882dc3331e66458babd3f6c32e19338
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-02-02 16:00:23 +0530

    Update set11.C

--- set11.C
+++ set11.C
@@ -3,12 +3,12 @@
 #include<math.h>
 void main()
 {
-int a,b,c
+int a,b,c;
 printf("enter the number":);
 scanf("%d",&a");
 printf("how much power do you want":);
 scanf("%d",&b);
 c=pwo(a,b);
-printf("the power value is %d");
+printf("the power value is %d",c);
 getch();
 }
