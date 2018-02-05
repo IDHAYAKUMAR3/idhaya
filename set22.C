ee7a6ebe17829084aa71c1352351abac5956cade
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-02-05 14:25:58 +0530

    Create set22.C

---
+++ set22.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[10],i,j,k,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+			k=a[i];
+			a[i]=a[j];
+			a[j]=t;
+			}
+		}
+	}
+	printf("%d",a[n-1]);
+	getch();
+}
