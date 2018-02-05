36167377b7671a93402548fc1b3f3b40df9000c2
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-02-05 13:47:59 +0530

    Update set16.C

--- set16.C
+++ set16.C
@@ -2,21 +2,21 @@
 
 void main()
 { 
-    int n,i,r,t;
+    int n,i,r,s;
     scanf("%d",&n);
     scanf("%d",&r);
     n++;
     while(n<r)
-    {   t=0;
+    {   s=0;
         for(i=2;i<n/2;i++)
         {
             if(n%i==0)
             {
-                t=1;
+                s=1;
                 break;
             }
         }
-        if(t==0)
+        if(s==0)
         printf("%d ",n);
         
         n++;
