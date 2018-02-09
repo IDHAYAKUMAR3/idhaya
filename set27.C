6b1541e6e296f650ef87aa98bfa97ca58e1ad9ea
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-02-09 16:17:14 +0530

    Create set27.C

---
+++ set27.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+char m;
+printf("\nEnter the number: ");
+scanf("%c",&m);
+if(m>='0'&&m<='9')
+{
+printf("\n YES");
+}
+else
+{
+printf("\n NO");
+}
+getch();
+}
