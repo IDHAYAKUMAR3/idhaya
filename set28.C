8939e59bd044fa3817223fda579b71628db5863d
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-02-09 16:21:24 +0530

    Create set28.C

---
+++ set28.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	int n,a[100],i;
+	printf("Enter the n value:\t");
+	scanf("%d\n",&n);
+	for(i=0;i<n;i++)
+	{
+	scanf("\n%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t%d\n",a[i],i);
+	}
+	getch();
+}
