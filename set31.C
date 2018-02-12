f12a90eb95ba30e4b955817499e149152df305b3
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-02-12 13:11:54 +0530

    Create set31.C

---
+++ set31.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+	int temp=0,i;
+	char t[50];
+	scanf("%[^\n]s",t);
+	for(i=0;t[i]!='\0';i++)
+		{
+		temp++;
+	}
+	printf("%d",temp);
+	getch();
+}
+
