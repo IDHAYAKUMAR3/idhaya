33ad964bdc351a08235af250cad0ecd91c52d7fb
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-02-21 14:11:16 +0530

    Create set41.C

---
+++ set41.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+	int a,i;
+	printf("how many time print");
+	scanf("%d",&a);
+	for(i=1;i<=a;i++)
+	{
+		printf("\n motimuthu");
+	}
+	getch();
+}
