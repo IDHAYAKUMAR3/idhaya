500a48ae2959f2f61a1c5673e471a7eb4f4c3d88
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-02-21 14:04:11 +0530

    Create set39.C

---
+++ set39.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int a[50],n,i,large;
+printf("Enter the no of elements:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+    
+}
+large=a[0];
+for(i=0;i<n;i++)
+{
+    if(a[i]>large)
+    large=a[i];
+}
+printf("The largest element is %d",large);
+getch();
+}
+
