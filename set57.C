6a40139b04428c3a1e7bffe846ba206bd6c7bf57
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-02-28 14:29:11 +0530

    Create set57.C

---
+++ set57.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+int z,b,c;
+printf("\n Enter two numbers:");
+scanf("%d%d",&z,&b);
+c=z;
+z=b;
+b=c;
+printf("\n After Swapping :%d %d",z,b);
+getch();
+}
