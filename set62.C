32ddbc533d5e837fa8bb19a06afefe052468ee81
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-03-02 16:04:21 +0530

    Create set62.C

---
+++ set62.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b[100],x=0,i;
+scanf("%d",&a);
+for(i=0;a>10;i++)
+{
+b[i]=a%10;
+a=a/10;
+if(b[i]!=0&&b[i]!=1)
+x++;
+}
+if(x>0)
+printf("NO");
+else
+printf("YES");
+}
