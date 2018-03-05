d30ce6756f6344a7a83f48660395636a4b3361f4
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-03-05 13:28:39 +0530

    Create set68.C

---
+++ set68.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int b;
+    printf("enter the number");
+    scanf("%d",&b);
+    if(b%7==0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
