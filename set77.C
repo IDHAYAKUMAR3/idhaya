bced821a645de439c6ce447d44fd255d52fb2a29
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-03-07 14:55:24 +0530

    Create set77.C

---
+++ set77.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,v;
+scanf("%d",&i);
+for(v=i;v<=i;v++);
+{
+    if(i%v==0)
+    {
+        printf("%d",v);
+        
+    }
+}
+getch();
+}
