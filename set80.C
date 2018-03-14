80717f1c8a9c927f7b6a48eca6a9e4b2ab5a6bd3
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-03-14 15:50:12 +0530

    Create set80.C

---
+++ set80.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+    char a[100];
+    int i,l;
+    scanf("%s",a);
+    l=strlen(a);
+    for(i=0;i<l;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            printf("%c ",a[i]);
+        }
+    }
+
+}
