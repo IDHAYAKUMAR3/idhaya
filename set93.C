f361a91b8b2397c41728f1bc28913723344bf2c7
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-03-21 15:50:49 +0530

    Create set93.C

---
+++ set93.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float a;
+printf("Enter the temperature in celcius :");
+scanf("%f",&a);
+printf("\nThe temperature is %f kelvin.",a+273);
+getch();
+}
