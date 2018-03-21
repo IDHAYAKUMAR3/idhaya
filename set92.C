3f4a13b7d8361655a833915b4c63daac56bc7870
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-03-21 15:47:32 +0530

    Create set92.C

---
+++ set92.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int z,i,a[10];
+int sum=0;
+printf("no.of elements:");
+scanf("%d",&z);
+printf("enter the elements:");
+for(i=0;i<z;i++)
+scanf("%d",&a[i]);
+{
+for(i=0;i<z;i++)
+sum=sum+a[i];
+printf("%d",sum);
+}
+getch();
+}
