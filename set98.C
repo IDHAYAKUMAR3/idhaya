cd6f407382200b500d40e651aaa025048d38336c
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-03-21 16:00:02 +0530

    Create set98.C

---
+++ set98.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+{
+int N,n[10],i,j;
+scanf("%d",&N);
+
+for(i=1;i<=N;i++)
+scanf("%d",&n[i]);
+for(i=1;i<=N;i++)
+{
+ for(j=i+1;j<=N;j++)
+ {
+  if(n[i]>n[j])
+   printf("%d",i);
+   }
+   }
+return 0;
+}
