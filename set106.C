ed3fe674482353c0626681805fffce1aa1c3c1d8
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-03-26 15:57:25 +0530

    Create set106.C

---
+++ set106.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int i,z=45;
+printf("enter the number:");
+scnaf("%d",&number);
+for(i=1;i<=26;i++)
+{
+z++
+}
+printf("%c",&z);
+getch();
+}
