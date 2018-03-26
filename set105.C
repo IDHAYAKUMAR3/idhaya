09bf1763bafe3d12ac285126210d76f8176da6bc
IDHAYAKUMAR3 <35688067+IDHAYAKUMAR3@users.noreply.github.com>
2018-03-26 15:54:18 +0530

    Create set105.C

---
+++ set105.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int i,a=45;
+printf("enter the number:");
+scnaf("%d",&number);
+for(i=1;i<=26;i++)
+{
+a++
+}
+printf("%c",&a);
+getch();
+}
